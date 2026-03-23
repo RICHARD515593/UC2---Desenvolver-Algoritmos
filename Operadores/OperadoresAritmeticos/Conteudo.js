b69af5192dd366c4e91be7cfb86f8242a01c2f9d
RICHARD DOS SANTOS PINHEIRO <04973951005@senacrs.edu.br>
2026-03-23 17:14:59 -0300

    Operadores

---
+++ Operadores/OperadoresAritmeticos/Conteudo.js
@@ -0,0 +1,63 @@
+//soma
+const primeiroValor = 10
+const segundoValor = 20
+
+const resultado = primeiroValor + segundoValor + 4
+
+console.log(resultado) //34
+
+//Subtração
+const primeiroValor2 = 10
+const segundoValor2 = 20
+
+const resultado2 = primeiroValor2 - segundoValor2 
+
+console.log(resultado2) //-10
+
+//Multiplicação
+
+const primeiroValor3 = 10
+const segundoValor3 = 25
+
+const resultado3 = primeiroValor3 * segundoValor3
+
+console.log(resultado3) //250
+
+//Divisão
+const primeiroValor4 = 345
+const segundoValor4 = 10
+
+const resultado4 = primeiroValor4 / segundoValor4
+
+console.log(resultado4) //34.5
+
+/*comparadores
+São eles:
+Igual:===
+Diferente:!==
+Maior e maior igual:> e >=
+Menor e menor igual:< e <= */
+
+"1" === "2" // false, são diferentes
+"2" === "2" // true, são iguais
+2 === "2" // false, tipo diferente
+
+const condicao = 1 === 2
+// o valor que sai da comparação
+// pode ser guardado em uma variavel
+// nesse caso, consicao === false
+
+"1" !== "2" //true, são diferentes
+"2" !== "2" //false, são iguais
+2 !== "2" // true, tipos diferentes
+
+const condicao - 1 !== 2 //true
+
+1 > 2 // false, porque 1 é menor que 2
+2 > 2 // false, porque 2 é igual a 2
+3 > 2 // true, porque 3 é maior que 2
+
+1 >= 2 // false, porque 1 é menor que 2
+2 >= 2 // true, porque 2 é igual a 2
+3 >= 2 // true, porque 3 é maior que 2
+
---
+++ Operadores/OperadoresAritmeticos/ex2.js
@@ -0,0 +1,6 @@
+const hamburg = 15
+const batata = 10
+
+const resultado = 2 * hamburg + batata
+
+console.log(resultado)
